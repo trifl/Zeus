98c303395336d765c53e79dac822ac7128471ac5
JP McGlone <jp@trifl.co>
2015-08-22 21:08:13 -0700

    first commit

---
+++ Zeus/Zeus.h
@@ -0,0 +1,19 @@
+//
+//  Zeus.h
+//  Zeus
+//
+//  Created by JP McGlone on 8/22/15.
+//  Copyright (c) 2015 Imgur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Zeus.
+FOUNDATION_EXPORT double ZeusVersionNumber;
+
+//! Project version string for Zeus.
+FOUNDATION_EXPORT const unsigned char ZeusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zeus/PublicHeader.h>
+
+
